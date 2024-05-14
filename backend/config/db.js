f32ba22200bf720f9093e71f1352d158c40b2c69
Khushwant <khushrawal17@gmail.com>
2024-05-14 16:07:12 +0530

    Ecommerce-project

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 
-// const mongoURI = "mongodb://127.0.0.1:27017/Ecommerce"
 
 async function connectToMongo() {
   await mongoose.connect(process.env.mongoURI).then(() => console.log("connected to Mongo Successfully")).catch(err => console.log(err)); 
